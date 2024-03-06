bd553bfd427a79dbb649232edeae5333e525d965
Yejin Woo <yejinwoo.me@gmail.com>
2024-03-06 22:07:39 +0900

    feat: setting base path as 'api'

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mysql = require("mysql2");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
+const cors = require("cors");
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.use(cors({
+  origin: "http://localhost:3000", // 접근 권한을 부여하는 도메인
+  credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+  optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+}));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -20,8 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// Routers
+const indexRouter = require("./routes/index");
+const recordRouter = require("./routes/record");
+const apiRouter = express.Router();
+
+// Attach existing routers to the base router
+// app.use("/", indexRouter);
+// app.use("/record", recordRouter);
+apiRouter.use("/", indexRouter); 
+apiRouter.use("/record", recordRouter);
+
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,7 +51,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-
 
 module.exports = app;
