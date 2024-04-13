cd92277d23e7ffa3e0fefaa7d307c91925cfa23e
Yejin Woo <yejinwoo.me@gmail.com>
2024-04-13 19:04:49 +0900

    chore: migration from express to nest.js

--- routes/record.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const recordController = require("../controllers/recordController");
-
-router.post("/", recordController.updateRecord);
-
-module.exports = router;
