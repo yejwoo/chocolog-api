e6411671f7bf9b2862bc8715df94127f796c0b0f
Yejin Woo <yejinwoo.me@gmail.com>
2024-04-14 19:00:28 +0900

    chore: create auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { LogService } from './log/log.service';
 import { LogController } from './log/log.controller';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 전역으로 환경변수 사용
     }),
+    AuthModule,
   ],
   controllers: [AppController, LogController],
   providers: [LogService],
